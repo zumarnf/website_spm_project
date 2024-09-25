a16eee60b2c0e865d9145a3e61f3d59ee6f20082
zumzum <kazonima@gmail.com>
2024-09-25 14:14:14 +0700

    updateprogress_penelitian_2

--- src/components/TablePen.jsx
+++ src/components/TablePen.jsx
@@ -10,7 +10,59 @@ const TablePen = () => {
         <MenuModal />
         <div className="h-full w-full border border-blckprmy rounded-xl p-4">
           <div className="px-7 py-4">
-            <h1 className="text-2xl font-bold pb-7 text-blckprmy">Dashboard</h1>
+            <h1 className="text-2xl font-bold text-blckprmy">
+              Data Penelitian
+            </h1>
+          </div>
+          <div>
+            <div className="overflow-x-auto text-blckprmy">
+              <table className="table">
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Job</th>
+                    <th>Favorite Color</th>
+                    <th>Favorite Color</th>
+                    <th>Favorite Color</th>
+                    <th>Favorite Color</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* row 1 */}
+                  <tr>
+                    <th>1</th>
+                    <td>Cy Ganderton</td>
+                    <td>Quality Control Specialist</td>
+                    <td>Blue</td>
+                    <td>Blue</td>
+                    <td>Blue</td>
+                    <td>Blue</td>
+                  </tr>
+                  {/* row 2 */}
+                  <tr>
+                    <th>2</th>
+                    <td>Hart Hagerty</td>
+                    <td>Desktop Support Technician</td>
+                    <td>Purple</td>
+                    <td>Blue</td>
+                    <td>Blue</td>
+                    <td>Blue</td>
+                  </tr>
+                  {/* row 3 */}
+                  <tr>
+                    <th>3</th>
+                    <td>Brice Swyre</td>
+                    <td>Tax Accountant</td>
+                    <td>Red</td>
+                    <td>Blue</td>
+                    <td>Blue</td>
+                    <td>Blue</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
